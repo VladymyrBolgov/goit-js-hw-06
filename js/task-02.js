2a168cee5494651f9163970afabac1481a7a32d8
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-07-08 10:36:06 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector('#ingredients');
+
+const elementsItems = arrays => arrays.map(item => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = item;
+  itemEl.classList.add('item');
+  return itemEl;
+});
+
+const ingredientsItems = elementsItems(ingredients);
+listEl.append(...ingredientsItems);
+
+console.log(listEl)
+
+
