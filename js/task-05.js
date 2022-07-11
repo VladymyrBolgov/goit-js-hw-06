34e554e9a6a34029783530de3d40702db7f89082
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-07-11 23:03:30 +0300

    5 доделал

--- js/task-05.js
+++ js/task-05.js
@@ -1,31 +1,8 @@
-// первый способ 
+// первый способ
 const input = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
 
 input.addEventListener('input', (event) => {
-    nameOutput.textContent = event.currentTarget.value
+  input.value === '' ?  nameOutput.textContent = 'Anonymous' : nameOutput.textContent = event.currentTarget.value
 })
-
-// второй способ
-/*const refs = {
-    input: document.querySelector('#name-input'),
-    nameOutput: document.querySelector('#name-output'),
-}
-refs.input.addEventListener('input', onInputChange);
-
-function onInputChange(event) {
-    refs.nameOutput.textContent = event.currentTarget.value;
-}*/
-
-// третий способ
-/*const input = document.querySelector('#name-input');
-const nameOutput = document.querySelector('#name-output');
-
-input.addEventListener('input', onInputChange)
-
-function onInputChange(event) {
-    nameOutput.textContent = event.currentTarget.value;
-}*/
-
-
 
