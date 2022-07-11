d07e8f4729751d3e3315cba0cefb71acee079f90
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-07-11 23:40:29 +0300

    задания скопировал

--- js/task-05.js
+++ js/task-05.js
@@ -6,3 +6,4 @@ input.addEventListener('input', (event) => {
   input.value === '' ?  nameOutput.textContent = 'Anonymous' : nameOutput.textContent = event.currentTarget.value
 })
 
+/**Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), подставляет его текущее значение в span#name-output. Если инпут пустой, в спане должна отображаться строка "Anonymous". */
