a7f7f7e225cfb943466f28bbf437f9daf848ed10
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-07-12 17:23:33 +0300

    t

--- js/task-10.js
+++ js/task-10.js
@@ -1,6 +1,14 @@
+
+/*const input = document.querySelector('input');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)}`;
+}*/
+
 
 
 
