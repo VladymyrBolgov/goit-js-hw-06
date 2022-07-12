937a42cd84d63578bf6bae7cb32908d8c71d84e7
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-07-12 16:26:23 +0300

    9 дз

--- js/task-09.js
+++ js/task-09.js
@@ -1,8 +1,19 @@
+//1
+const changeColorBtn = document.querySelector('button')
+const body = document.querySelector('body')
+const spanColor = document.querySelector('span')
+
+changeColorBtn.addEventListener('click', clickchangeColorBtn)
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+function clickchangeColorBtn() {
+  body.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = getRandomHexColor()
+}
 
-
+//
 /**Напиши скрипт, который изменяет цвета фона 
   элемента <body> через инлайн стиль при клике 
   на button.change-color и выводит значение цвета в span.color.
@@ -18,3 +29,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 } */
+
+
+/**function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const body = document.querySelector('body')
+const changeColorBtn = document.querySelector('button')
+const spanColor = document.querySelector('span')
+
+const clickchangeColorBtn = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = body.style.backgroundColor;
+}
+changeColorBtn.addEventListener('click', clickchangeColorBtn) */
