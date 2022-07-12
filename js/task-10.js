61fa25ee1c797c167088e7bbe04d36c69950521b
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-07-12 20:27:30 +0300

    10

--- js/task-10.js
+++ js/task-10.js
@@ -1,14 +1,39 @@
-
-/*const input = document.querySelector('input');
 const createButton = document.querySelector('[data-create]');
 const destroyButton = document.querySelector('[data-destroy]');
+const inputEl = document.querySelector('input');
+const listEl = document.querySelector('#controls')
+const containerEl = document.querySelector('#boxes')
 
+createButton.addEventListener('click', () => createBoxes(inputEl.value));
+destroyButton.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  let size = 20;
+  const boxEl = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    size = size + 10;
+    const box = document.createElement('div');
+    box.style.backgroundColor = `${getRandomHexColor()}`
+    box.style.width = size + 'px'
+    box.style.height = size + 'px'
+    box.classList.add('box-item')
+    boxEl.push(box)
+  }
+  containerEl.append(...boxEl)
+  return containerEl
+}
+
+function destroyBoxes() {
+  containerEl.innerHTML = "";
+  inputEl.value = "";
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)}`;
-}*/
-
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
 
 
@@ -29,7 +54,8 @@ function getRandomHexColor() {
 сколько указано в amount и добавляет их в div#boxes.
 
 Размеры самого первого <div> - 30px на 30px.
-Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
+Каждый элемент после первого, должен быть шире и выше предыдущего 
+на 10px.
 Все элементы должены иметь случайный цвет фона в формате HEX. 
 Используй готовую функцию getRandomHexColor для получения цвета.
 function getRandomHexColor() {
@@ -37,5 +63,5 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, 
-тем самым удаляя все созданные элементы. */
+Создай функцию destroyBoxes(), которая очищает содержимое 
+div#boxes, тем самым удаляя все созданные элементы. */
