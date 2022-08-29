3baee90144d883a10d9009b463f62e71b9c308dd
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-08-29 23:58:52 +0300

    5

--- js/task-04.js
+++ js/task-04.js
@@ -1,4 +1,4 @@
-
+/*
     const decrementBtn = document.querySelector('[data-action="decrement"]')
     const incrementBtn = document.querySelector('[data-action="increment"]')
     const counterEl = document.querySelector('#value')
@@ -7,18 +7,24 @@
     decrementBtn.addEventListener('click', () => {
         counterEl.textContent = counterValue -= 1;
     })
-
     incrementBtn.addEventListener('click', () => {
         counterEl.textContent = counterValue += 1;
     })
-
-/**Счетчик состоит из спана и кнопок, которые, при клике, должны увеличивать и уменьшать его значение на единицу.
+*/
+/**Счетчик состоит из спана и кнопок, которые, 
+ * при клике, должны увеличивать и уменьшать его
+ *  значение на единицу.
 
 <div id="counter">
   <button type="button" data-action="decrement">-1</button>
   <span id="value">0</span>
   <button type="button" data-action="increment">+1</button>
 </div>
-1. Создай переменную counterValue в которой будет храниться текущее значение счетчика и инициализируй её значением 0.
-2. обавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
-3. Обновляй интерфейс новым значением переменной counterValue. */
+1. Создай переменную counterValue в которой
+будет храниться текущее значение счетчика и
+ инициализируй её значением 0.
+2. Добавь слушатели кликов на кнопки, внутри 
+которых увеличивай или уменьшай значение счетчика.
+3. Обновляй интерфейс новым значением 
+переменной counterValue. */
+
