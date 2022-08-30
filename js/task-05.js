e7625f05fc1c84e408c8d521070b3eb486683ed1
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-08-30 22:58:17 +0300

    u

--- js/task-05.js
+++ js/task-05.js
@@ -1,13 +1,22 @@
-
+/*
 const input = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
 
 input.addEventListener('input', (event) => {
-  input.value === '' ?  nameOutput.textContent = 'Anonymous' : nameOutput.textContent = event.currentTarget.value
+  input.value === '' ?  nameOutput.textContent = 'Anonymous' 
+  : nameOutput.textContent = event.currentTarget.value
 })
-
+*/
 /**Напиши скрипт который, при наборе текста
  *  в инпуте input#name-input (событие input),
  *  подставляет его текущее значение в span#name-output. 
  * Если инпут пустой, в спане должна 
  * отображаться строка "Anonymous". */
+
+const input = document.querySelector('#name-input')
+const nameOutput = document.querySelector('#name-output')
+
+input.addEventListener('input', event => {
+  input.value === '' ? nameOutput.textContent = 'Anonymous'
+    : nameOutput.textContent = event.currentTarget.value
+})
